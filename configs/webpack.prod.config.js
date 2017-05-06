01b3848afc11196462799e7ef4392a840b47e2be
Mohith G <mohithgm@gmail.com>
2017-05-06 14:45:44 +0530

    long term caching in webpack

---
+++ configs/webpack.prod.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ChunkManifestPlugin = require("chunk-manifest-webpack-plugin");
+const WebpackChunkHash = require("webpack-chunk-hash");
+const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
+
+const config = {
+  entry: {
+    app: './app/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].[chunkhash].js',
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: 'babel-loader' }
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module) {
+        // this assumes your vendor imports exist in the node_modules directory
+        return module.context && module.context.indexOf('node_modules') !== -1;
+      }
+    }),
+    //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+    }),
+    new webpack.HashedModuleIdsPlugin(),
+    new WebpackChunkHash(),
+    new ChunkManifestPlugin({
+      filename: "chunk-manifest.json",
+      manifestVariable: "webpackManifest"
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './app/index.ejs'
+    }),
+    new InlineManifestWebpackPlugin({
+      name: 'webpackManifest'
+    })
+  ]
+};
+
+module.exports = config;
