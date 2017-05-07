688b0bab3937d8f79ad0516e3866f7c598a492da
Mohith G <mohithgm@gmail.com>
2017-05-07 14:08:42 +0530

    Use linting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    rules: {
+        "react/prefer-stateless-function": 0,
+        "no-else-return": 0,
+        "react/jsx-filename-extension": 0,
+    },
+    globals: {
+        _: true,
+        React: true,
+        document: true,
+    }
+};
